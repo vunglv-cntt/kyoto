0a826837a6152847844b421625c4c482c016d908
Vững Lê Văn <levanvung113@gmail.com>
2023-09-12 13:53:36 +0700

    - Update Banner

--- src/components/HomePage/Banners.tsx
+++ src/components/HomePage/Banners.tsx
@@ -2,13 +2,14 @@
 import Carousel from "@component/carousel-cards/Carousel";
 // import Carousel from "@component/carousel/Carousel";
 // import Container from "@component/Container";
-import {Image} from "@component/image";
+import { Image } from "@component/image";
 import { useAsync } from "@hooks/useAsync";
 
 import React, { useMemo } from "react";
 import { apiGetBanners } from "services/home";
 import { Banner } from "services/main/models";
 import StyledBanners from "./homepagecss/Banners.style";
+import { fontWeight } from "styled-system";
 
 const Banners: React.FC = () => {
   const [, bannerData] = useAsync(apiGetBanners, {
@@ -25,7 +26,12 @@ const Banners: React.FC = () => {
       })) || [],
     [bannerData]
   );
-
+  const titleStyled = {
+    fontWeight: "Montserrat",
+    fontSize: "60px",
+    color :"white",
+    marginLeft : "82px"
+  };
   return (
     <StyledBanners className="mb-4">
       <div className="max-w-screen-2xl mx-auto relative">
@@ -44,7 +50,7 @@ const Banners: React.FC = () => {
           <div>
             <div>
               {" "}
-              <h1 className="banner-text">
+              <h1 style={titleStyled}>
                 KYOTO - NÔNG NGHIỆP <br /> CÔNG NGHỆ
               </h1>{" "}
             </div>
