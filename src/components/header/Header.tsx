a531e7602882ee910d338085cc7330f186b0927e
Vững Lê Văn <levanvung113@gmail.com>
2023-09-12 10:38:45 +0700

    - Update UI

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,49 +1,70 @@
-import { Image } from "@component/image";
-import Link from "next/link";
-// import Container from "../Container";
-import FlexBox from "../FlexBox";
+// import Container from "../Container"; 
 import StyledHeader from "./HeaderStyle";
-import TopbarHeader from "@component/topbar/TopbarHeader";
-import Navbar from "@component/navbar/Navbar";
+import React from "react";
+import { Row, Col, Menu, Typography, Button } from "antd";
+import {
+  HomeOutlined,
+  SafetyCertificateOutlined,
+  TeamOutlined,
+  ReadOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
+
 type HeaderProps = {
   isFixed?: boolean;
   className?: string;
 };
+const { Title } = Typography;
 
-const Header: React.FC<HeaderProps> = ({  className }) => {
+const Header: React.FC<HeaderProps> = ({ className }) => {
   return (
     <StyledHeader className={className}>
-      <div>
-        <div
-          style={{ height: "100%", display: "flex", flexDirection: "column" }}
-        >
-          <div
-            className="navbarStyleLogoKyoto"
-            style={{ flex: "1", display: "flex" }}
-          >
-            <div className="logo">
-              <FlexBox className="logo" alignItems="center" mr="1rem">
-                <Link href="/">
-                  <a>
-                    <Image src="/assets/images/logokyoto.svg" alt="logo" />
-                  </a>
-                </Link>
-              </FlexBox>
-            </div>
-
-            <div className="navbarStyleText-1"  >
-              <FlexBox justifyContent="center">
-                <TopbarHeader />
-              </FlexBox>
-            </div>
-          </div>
-
-          <div className="navbarStyleText" style={{ flex: "1" }}>
-            <FlexBox justifyContent="center">
-              <Navbar />
-            </FlexBox>
-          </div>
-        </div>
+      <div className="header">
+        <Row justify="space-between" align="middle">
+          <Col span={12}>
+            <Row justify="start" align="middle">
+              <Col span={4}>
+                <Title level={3}>Logo</Title>
+              </Col>
+              <Col span={16}>
+                <Menu mode="horizontal" theme="dark" className="top-menu">
+                  <Menu.Item key="location">Địa điểm</Menu.Item>
+                  <Menu.SubMenu title="Ngôn ngữ">
+                    <Menu.Item key="english">Tiếng Anh</Menu.Item>
+                    <Menu.Item key="vietnamese">Tiếng Việt</Menu.Item>
+                    <Menu.Item key="french">Tiếng Pháp</Menu.Item>
+                  </Menu.SubMenu>
+                  <Menu.Item key="login">
+                    <Button type="primary">Đăng nhập</Button>
+                  </Menu.Item>
+                  <Menu.Item key="register">
+                    <Button type="primary">Đăng ký</Button>
+                  </Menu.Item>
+                </Menu>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={12}>
+            <Menu mode="horizontal" theme="dark" defaultSelectedKeys={["home"]}>
+              <Menu.Item key="home" icon={<HomeOutlined />}>
+                Trang chủ
+              </Menu.Item>
+              <Menu.Item key="warranty" icon={<SafetyCertificateOutlined />}>
+                Bảo hành
+              </Menu.Item>
+              <Menu.Item key="agents" icon={<TeamOutlined />}>
+                Hệ thống đại lý
+              </Menu.Item>
+              <Menu.Item key="news" icon={<ReadOutlined />}>
+                Tin tức
+              </Menu.Item>
+              <Menu.Item key="jobs" icon={<PhoneOutlined />}>
+                Tuyển dụng
+              </Menu.Item>
+              <Menu.Item key="contact">Liên hệ</Menu.Item>
+            </Menu>
+          </Col>
+        </Row>
       </div>
     </StyledHeader>
   );
